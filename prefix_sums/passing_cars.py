f4e770348d980373ca6c245e7246cb5765ffaa5d
Christian Gutierrez <chesstrian@gmail.com>
2019-03-06 11:55:13 -0500

    Prefix sums lesson

---
+++ prefix_sums/passing_cars.py
@@ -0,0 +1,14 @@
+def solution(a):
+    n = len(a)
+    p = [0] * (n + 1)
+
+    result = 0
+
+    for i in range(1, n + 1):
+        p[i] = p[i - 1] + a[i - 1]
+
+    for i in range(n):
+        if a[i] == 0:
+            result += p[n] - p[i + 1]
+
+    return -1 if result > 1000000000 else result
